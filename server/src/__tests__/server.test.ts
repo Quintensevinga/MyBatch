44ca4e1d7521fa692cb4a89d487db794ce5ecc5b
Vipin Devan <vipindevan08@gmail.com>
2023-06-17 12:57:55 +0200

    testing our-recipes

--- server/src/__tests__/server.test.ts
+++ server/src/__tests__/server.test.ts
@@ -4,11 +4,13 @@ import { createServer } from '../server';
 import { beerRecipe, myRecipe, addIngredient } from '../models/models';
 
 beforeEach(async () => {
-  await mongoose.connect('mongodb://127.0.0.1:27017/mybatch');
+  await mongoose.connect('mongodb://127.0.0.1:27017/checking');
 });
 
 /* Closing database connection after each test. */
 afterEach(async () => {
+  await addIngredient.deleteMany();
+  await beerRecipe.deleteMany();
   await mongoose.connection.close();
 });
 
@@ -27,13 +29,13 @@ test('GET /ingredients', async () => {
     .then((response) => {
       // Check the response type and length
       expect(Array.isArray(response.body)).toBeTruthy();
-      //   expect(response.body.length).toEqual(1);
+      expect(response.body.length).toEqual(1);
 
       // Check the response data
-      expect(response.body[response.body.length - 1]._id).toBe(ingredient.id);
-      expect(response.body[response.body.length - 1].name).toBe(ingredient.name);
-      expect(response.body[response.body.length - 1].amount).toBe(ingredient.amount);
-      expect(response.body[response.body.length - 1].type).toBe(ingredient.type);
+      expect(response.body[0]._id).toBe(ingredient.id);
+      expect(response.body[0].name).toBe(ingredient.name);
+      expect(response.body[0].amount).toBe(ingredient.amount);
+      expect(response.body[0].type).toBe(ingredient.type);
     });
 });
 
@@ -61,5 +63,79 @@ test('POST /ingredients', async () => {
       expect(oneIngredient?.name).toBe(data.name);
       expect(oneIngredient?.amount).toBe(data.amount);
       expect(oneIngredient?.type).toBe(data.type);
+    });
+});
+
+test('DELETE inventory/:id', async () => {
+  const post = await addIngredient.create({
+    name: 'Mcheck',
+    amount: 'Loremipsum',
+    type: 'soft',
+  });
+
+  await supertest(app)
+    .delete('/inventory/' + post.id)
+    .expect(200)
+    .then(async () => {
+      expect(await addIngredient.findOne({ _id: post.id })).toBeFalsy();
+    });
+});
+
+test('GET /ourrecipes', async () => {
+  const ingredient = await beerRecipe.create({
+    name: 'Pilsnersds',
+    style: 'Pilsner',
+    description: 'A crisp and light-bodied beer with a clean malt profile and floral hop aroma.',
+    batchSize: '20 liters',
+    ingredients: {
+      malts: [
+        {
+          name: 'Pilsner Malt',
+          amount: '4 kg',
+        },
+      ],
+      hops: [
+        {
+          name: 'Saaz',
+          amount: '30 g',
+          time: '60 minutes',
+        },
+        {
+          name: 'Saaz',
+          amount: '20 g',
+          time: '15 minutes',
+        },
+        {
+          name: 'Saaz',
+          amount: '20 g',
+          time: '5 minutes',
+        },
+      ],
+      yeast: 'Czech Lager',
+    },
+    instructions: [
+      'Mash the Pilsner malt with hot water at 65°C for 60 minutes.',
+      'Boil the wort and add Saaz hops for bittering at 60 minutes.',
+      'Add Saaz hops at 15 minutes remaining in the boil.',
+      'Add Saaz hops at 5 minutes remaining in the boil.',
+      'Cool the wort and transfer to a fermentation vessel.',
+      'Pitch the Czech Lager yeast and ferment at 10-12°C for one to two weeks.',
+      'Bottle or keg the beer, carbonate, and enjoy!',
+    ],
+  });
+
+  await supertest(app)
+    .get('/our-recipes')
+    .expect(200)
+    .then((response) => {
+      // Check the response type and length
+      expect(Array.isArray(response.body)).toBeTruthy();
+      expect(response.body.length).toEqual(1);
+
+      // Check the response data
+      expect(response.body[0]._id).toBe(ingredient.id);
+      expect(response.body[0].name).toBe(ingredient.name);
+      expect(response.body[0].style).toBe(ingredient.style);
+      expect(response.body[0].description).toBe(ingredient.description);
     });
 });
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,7 +3,7 @@ import { Express, Request, Response } from 'express';
 import { getAllIngredients, createIngredients, deleteIngredient, getOurRecipes, getMyRecipes, postMyRecipe } from './controllers';
 
 function routes(app: Express) {
-  app.get('/heal', (req: Request, res: Response) => {
+  app.get('/health', (req: Request, res: Response) => {
     res.sendStatus(200);
   });
   app.get('/users', (req: Request, res: Response): void => {
